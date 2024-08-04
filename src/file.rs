8d2fb2a7eeb369f0799a25bab6872e948ad006c2
ErikaGenova <94406079+ErikaGenova@users.noreply.github.com>
2024-08-04 11:25:31 +0200

    Fix for No extention and sub-folders
    Copies the sub-folders and if Configuration has no extention copies everything

--- src/file.rs
+++ src/file.rs
@@ -5,25 +5,37 @@ use crate::configuration::Configuration;
 pub fn copy_files_with_extention(config: Configuration) -> Result<(), std::io::Error> {
     let src_path = Path::new(&config.source_path);
     let dest_path = Path::new(&config.destination_path);
-    let ext = config.extension_filter.as_ref().unwrap();
+    let ext = config.extension_filter.as_ref();
 
     if !src_path.exists() {
         return Err(std::io::Error::new(std::io::ErrorKind::NotFound, "Source path does not exist"));
     }
 
     if !dest_path.exists() {
-        fs::create_dir_all(dest_path)?;
+        fs::create_dir_all(&dest_path)?;
     }
 
-    for entry in fs::read_dir(src_path)? {
+    for entry in fs::read_dir(&src_path)? {
         let entry = entry?;
         let path = entry.path();
-        if path.is_file() {
+        if path.is_dir() {
+            let new_dest_path = dest_path.join(entry.file_name());
+            let new_config = Configuration::new(config.shape.clone(), path.to_str().unwrap().to_string(), new_dest_path.to_str().unwrap().to_string(), config.extension_filter.clone());
+            copy_files_with_extention(new_config)?;
+        } else if path.is_file() {
             if let Some(file_name) = path.file_name() {
                 if let Some(file_name) = file_name.to_str() {
-                    if file_name.ends_with(ext) {
-                        let dest_file = dest_path.join(file_name);
-                        fs::copy(&path, &dest_file)?;
+                    match ext {
+                        Some(ext) => {
+                            if file_name.ends_with(ext) {
+                                let dest_file = dest_path.join(file_name);
+                                fs::copy(&path, &dest_file)?;
+                            }
+                        },
+                        None => {
+                            let dest_file = dest_path.join(file_name);
+                            fs::copy(&path, &dest_file)?;
+                        },
                     }
                 }
             }
@@ -32,7 +44,6 @@ pub fn copy_files_with_extention(config: Configuration) -> Result<(), std::io::E
 
     Ok(())
 }
-
 #[cfg(test)]
 mod tests {
     use crate::pattern_recognition::Shape;
@@ -44,6 +55,15 @@ mod tests {
         let dest = r"C:\Users\erika\Desktop\PoliTo\Programmazione di sistema\TEST DESTINAZIONE FILE";
         let ext = "txt";
         let config = Configuration::new(Shape::Circle, src.to_string(), dest.to_string(), Some(ext.to_string()));
+        let result = copy_files_with_extention(config);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_copy_every_file() {
+        let src = r"C:\Users\erika\Desktop\PoliTo\Programmazione di sistema\TEST COPIA SORGENTE FILE";
+        let dest = r"C:\Users\erika\Desktop\PoliTo\Programmazione di sistema\TEST DESTINAZIONE FILE NO EXTENSION";
+        let config = Configuration::new(Shape::Circle, src.to_string(), dest.to_string(), None);
         let result = copy_files_with_extention(config);
         assert!(result.is_ok());
     }
