a92e4c47fe6a6cc280d6db888826e3b2155e66f4
Andrea Di Battista <71295336+DibaLt@users.noreply.github.com>
2024-08-29 20:06:39 +0200

    Sounds works

--- src/sounds.rs
+++ src/sounds.rs
@@ -1,16 +1,21 @@
 use std::io::Cursor;
 use rodio::{Decoder, OutputStream, Source};
+use std::thread::sleep;
+use std::time::Duration;
 
 pub fn use_audio() {
 
     let audio_data = include_bytes!("../sounds/activation.wav");
+
 
     // Set up the audio output
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 
     // Decode and play the embedded audio
     let cursor = Cursor::new(audio_data.as_ref());
+
     let source = Decoder::new(cursor).unwrap();
     stream_handle.play_raw(source.convert_samples()).unwrap();
+    sleep(Duration::from_secs(2));
 
 }
