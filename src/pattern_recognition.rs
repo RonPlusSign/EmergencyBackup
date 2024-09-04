94b1b1d6a8b973000b9a504f745413da6cb0c4e9
Andrea Delli <andrea.delli2001@gmail.com>
2024-09-04 22:50:03 +0200

    refactor: remove unused code

--- src/pattern_recognition.rs
+++ src/pattern_recognition.rs
@@ -6,9 +6,6 @@ use std::time::Duration;
 
 use guessture::{find_matching_template_with_defaults, Path2D, Template};
 use mouse_position::mouse_position::Mouse;
-use plotters::drawing::IntoDrawingArea;
-use plotters::style::Color;
-use plotters::style::colors::colormaps::*;
 use serde::{Deserialize, Serialize};
 
 const POINTS_PER_FIGURE: usize = 200;   // Maximum number of points to store
@@ -237,90 +234,6 @@ pub fn cancel_template(invert_direction: bool) -> Template {
     Template::new("Cancel".to_string(), &cancel_points).unwrap()
 }
 
-/// Draw a shape using plotters
-///
-/// #### Arguments
-/// * `shape`: Path2D representing the shape
-/// * `title`: File name to save the plot
-pub fn draw_shape(shape: Path2D, title: String) {
-    // Find the bounding box of the shape
-    let mut min_x = f32::MAX;
-    let mut max_x = f32::MIN;
-    let mut min_y = f32::MAX;
-    let mut max_y = f32::MIN;
-    for (x, y) in shape.points() {
-        if x < min_x { min_x = x; }
-        if x > max_x { max_x = x; }
-        if y < min_y { min_y = y; }
-        if y > max_y { max_y = y; }
-    }
-
-    let root = plotters::prelude::BitMapBackend::new(&title, (800, 800)).into_drawing_area();
-    root.fill(&plotters::prelude::WHITE).unwrap();
-
-    let mut chart = plotters::chart::ChartBuilder::on(&root)
-        .x_label_area_size(40)
-        .y_label_area_size(40)
-        .margin(10)
-        .build_cartesian_2d(min_x as f64..max_x as f64, min_y as f64..max_y as f64)
-        .unwrap();
-    chart.configure_mesh().draw().unwrap();
-    let points = shape.points().iter().cloned().collect::<Vec<(f32, f32)>>();
-    chart.draw_series(points.iter()
-        .enumerate()
-        .map(|(index, (x, y))| plotters::prelude::Circle::new((*x as f64, *y as f64), 5, VulcanoHSL.get_color(index as f64 / points.len() as f64).filled())))
-        .unwrap();
-}
-
-/// Draw multiple shapes in the same plot using plotters
-///
-/// #### Arguments
-/// * `shapes`: List of Path2D representing the shapes
-/// * `title`: File name to save the plot
-pub fn draw_multiple_shapes(shapes: Vec<Path2D>, title: String) {
-    // Find the bounding box of the shape
-    let mut min_x = f32::MAX;
-    let mut max_x = f32::MIN;
-    let mut min_y = f32::MAX;
-    let mut max_y = f32::MIN;
-    for shape in &shapes {
-        for (x, y) in shape.points() {
-            if x < min_x { min_x = x; }
-            if x > max_x { max_x = x; }
-            if y < min_y { min_y = y; }
-            if y > max_y { max_y = y; }
-        }
-    }
-
-    let root = plotters::prelude::BitMapBackend::new(&title, (800, 800)).into_drawing_area();
-    root.fill(&plotters::prelude::WHITE).unwrap();
-
-    let mut chart = plotters::chart::ChartBuilder::on(&root)
-        .x_label_area_size(40)
-        .y_label_area_size(40)
-        .margin(10)
-        .build_cartesian_2d(min_x as f64..max_x as f64, min_y as f64..max_y as f64)
-        .unwrap();
-    chart.configure_mesh().draw().unwrap();
-
-    enum ColorPalette { VulcanoHSL(VulcanoHSL), MandelbrotHSL(MandelbrotHSL), ViridisRGB(ViridisRGB), Copper(Copper), Bone(Bone) }
-    let palettes: Vec<ColorPalette> = vec![ColorPalette::VulcanoHSL(VulcanoHSL), ColorPalette::MandelbrotHSL(MandelbrotHSL), ColorPalette::ViridisRGB(ViridisRGB), ColorPalette::Copper(Copper), ColorPalette::Bone(Bone)];
-
-    for (shape_index, shape) in shapes.iter().enumerate() {
-        let points = shape.points().iter().cloned().collect::<Vec<(f32, f32)>>();
-        chart.draw_series(points.iter()
-            .enumerate()
-            .map(|(index, (x, y))| plotters::prelude::Circle::new((*x as f64, *y as f64), 5, match &palettes[shape_index % palettes.len()] {
-                ColorPalette::VulcanoHSL(palette) => palette.get_color(index as f64 / points.len() as f64).filled(),
-                ColorPalette::MandelbrotHSL(palette) => palette.get_color(index as f64 / points.len() as f64).filled(),
-                ColorPalette::ViridisRGB(palette) => palette.get_color(index as f64 / points.len() as f64).filled(),
-                ColorPalette::Copper(palette) => palette.get_color(index as f64 / points.len() as f64).filled(),
-                ColorPalette::Bone(palette) => palette.get_color(index as f64 / points.len() as f64).filled(),
-            })))
-            .unwrap();
-    }
-}
-
 /// Check if all the points are similar (within a threshold). If the points are similar, the mouse is not moving enough.
 ///
 /// #### Arguments
@@ -349,54 +262,6 @@ pub fn all_points_near(points: &VecDeque<Mouse>) -> bool {
     true
 }
 
-/// Convert a list of points to a scaled&centered Path2D
-/// ### Arguments
-/// * `points`: A list of points representing the mouse positions
-/// * `max_range_for_dimensions`: maximum variation for the dimensions
-/// ### returns
-/// Path2D containing the scaled points
-pub fn points_to_path(points: &VecDeque<Mouse>, max_range_for_dimensions: i32) -> Path2D {
-    // The path is scaled to fit within the maximum dimension (range [-max_dimension/2, max_dimension/2], centered at (0, 0))
-
-    let mut path = Path2D::default();
-    let mut min_x = f32::MAX;
-    let mut max_x = f32::MIN;
-    let mut min_y = f32::MAX;
-    let mut max_y = f32::MIN;
-    let mut mean_x = 0.0;
-    let mut mean_y = 0.0;
-
-    // Find the bounding box of the points and calculate the mean
-    for point in points {
-        match *point {
-            Mouse::Position { x, y } => {
-                if x < min_x as i32 { min_x = x as f32; }
-                if x > max_x as i32 { max_x = x as f32; }
-                if y < min_y as i32 { min_y = y as f32; }
-                if y > max_y as i32 { max_y = y as f32; }
-                mean_x += x as f32;
-                mean_y += y as f32;
-            }
-            Mouse::Error => { return path; }
-        }
-    }
-    mean_x /= points.len() as f32;
-    mean_y /= points.len() as f32;
-
-    // Scale the points to fit within the maximum dimension
-    let scale = max_range_for_dimensions as f32 / (max_x - min_x).max(max_y - min_y);
-    for point in points {
-        match *point {
-            Mouse::Position { x, y } => {
-                let x = (x as f32 - mean_x) * scale;
-                let y = (y as f32 - mean_y) * scale;
-                path.push(x, y);
-            }
-            Mouse::Error => { return path; }
-        }
-    }
-    path
-}
 
 pub fn wait_for_symbol(templates: &Vec<Template>, stop_condition: Arc<Mutex<bool>>) -> Option<Shape> {
     let mut points = VecDeque::new(); // Circular buffer: a point is added at the end and removed from the front
@@ -423,30 +288,5 @@ pub fn wait_for_symbol(templates: &Vec<Template>, stop_condition: Arc<Mutex<bool
         }
 
         sleep(Duration::from_millis(mouse_sampling_time_ms));
-    }
-}
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_draw_shape() {
-        let shape = circle_template(false);
-        draw_shape(shape.path.clone(), "circle.png".to_string());
-    }
-
-    #[test]
-    fn test_draw_multiple_shapes() {
-        let shapes = vec![circle_template(false).path.clone(), square_template(false).path.clone(), triangle_template(false).path.clone()];
-        draw_multiple_shapes(shapes, "shapes.png".to_string());
-    }
-
-    #[test]
-    fn test_points_to_path() {
-        let points: VecDeque<Mouse> = vec![Mouse::Position { x: 0, y: 0 }, Mouse::Position { x: 1, y: 1 }, Mouse::Position { x: 2, y: 2 }].into_iter().collect();
-        let path = points_to_path(&points, 100);
-        assert_eq!(path.points().len(), 3);
     }
 }
